c88aaa417369680be0427b9e41e099bdb605000f
aigul-ermak <aigulermak@gmail.com>
2021-11-02 15:38:12 -0300

    Clear button in Search and refactoring

--- src/App.js
+++ src/App.js
@@ -35,12 +35,17 @@ class App extends Component {
   clearUsers = () => this.setState({ users: [], loading: false });
 
   render() {
+    const { users, loading } = this.state;
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -11,6 +11,7 @@ export class Search extends Component {
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
   };
   onSubmit = (e) => {
     e.preventDefault();
@@ -21,6 +22,7 @@ export class Search extends Component {
     }
   };
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form className='form' onSubmit={this.onSubmit} className='form'>
@@ -37,12 +39,11 @@ export class Search extends Component {
             className='btn btn-dark btn-block'
           />
         </form>
-        <button
-          className='btn btn-light btn-block'
-          onClick={this.props.clearUsers}
-        >
-          Clear
-        </button>
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
